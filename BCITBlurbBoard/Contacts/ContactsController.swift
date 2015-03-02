595817da73154b58f0ce4a386fa504b2e8aa15b3
inderjeet khangura <ikhangura@gmail.com>
2015-03-02 14:42:50 -0800

    still working

--- BCITBlurbBoard/Contacts/ContactsController.swift
+++ BCITBlurbBoard/Contacts/ContactsController.swift
@@ -8,20 +8,40 @@
 
 
 import UIKit
-class ContactsController: UIViewController {
+class ContactsController: UIViewController , UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
     
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        //self.tableview.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
     
     @IBAction func btnBackPressed(sender: UIButton) {
         self.dismissViewControllerAnimated(true, completion: nil);
     }
+    
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+    cell.textLabel?.text = "Hi"
+    
+    return cell
+    }
+  
+    
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
 }
 
